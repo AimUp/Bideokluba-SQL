97d04d55ca795d882ac7a580ceb959ca6514a230
Hector Vadillo <vadillo.hector@gmail.com>
2016-05-02 14:03:58 +0200

    jyf

--- src/db/proiektua/logic/Administratzailea.java
+++ src/db/proiektua/logic/Administratzailea.java
@@ -1,24 +1,32 @@
 package db.proiektua.logic;
 
+import db.proiektua.db.Aginduak;
+import db.proiektua.db.DatuBasea;
+
 public class Administratzailea extends Erabiltzailea{
+	
+	private Aginduak agindua = new Aginduak();
 	
 	public Administratzailea(int pIdBazkidea, String pErabiltzailea, String pPasahitza, String pIzena, String pAbizena, String pHelbidea){
 		super(pIdBazkidea, pErabiltzailea, pPasahitza, pIzena, pAbizena, pHelbidea);
 	}
 	
-	public void bazkideBerriaSortu(){
-		
+	public void bazkideBerriaSortu(String pPasahitza, String pErabiltzaile, String pIzena, String pAbizena, String pHelbidea, int pKreditua, boolean pEgoera, String pBazNoiztik){
+		agindua.createBazkidea(pPasahitza, pErabiltzaile, pIzena, pAbizena, pHelbidea, pKreditua, pEgoera, pBazNoiztik);
+		//DatuBasea.getDatuBasea().getDatuBasea().getQuery("insert into Bazkidea values ("pPasahitza", "pIzena", "pAbizena", "pHelbidea", "pKreditua", "pEgoera", "pBazNoiztik")");
 	}
 	
-	public void bazkideBatenEgoeraAldatu(){
-		
+	public void bazkideBatenEgoeraAldatu(String pErabiltzaile, boolean pEgoera){
+		agindua.bazkideBatenEgoeraAldatu(pErabiltzaile, pEgoera);
 	}
 	
-	public void pelikulaBerriBatSartu(){
-		
+	public void pelikulaBerriBatSartu(String pIzenurua, int pPrezioa, String pEgoera, String pData){
+		agindua.pelikulaBerriBatSartu(pIzenurua, pPrezioa, pEgoera, pData);
 	}
 	
-	public void pelikulariBajaEman(){
-		
+	public void pelikulariBajaEman(String pIzenburua){
+		Pelikula pelikula = agindua.bilatuPelikula(pIzenburua);
+		pelikula.deskatalogatu();
 	}
 }
+
--- src/db/proiektua/logic/Bazkidea.java
+++ src/db/proiektua/logic/Bazkidea.java
@@ -1,27 +1,42 @@
 package db.proiektua.logic;
 
+import db.proiektua.db.Aginduak;
+
 public class Bazkidea extends Erabiltzailea{
 
 	private String bezeroaNoiztik;
+	private Aginduak agindua = new Aginduak();
 	
 	public Bazkidea(int pIdBazkidea, String pErabiltzailea, String pPasahitza, String pIzena, String pAbizena, String pHelbidea){
 		super(pIdBazkidea, pErabiltzailea, pPasahitza, pIzena, pAbizena, pHelbidea);
 	}
 	
 	public void datuPertsonalakAldatu(){
-		
+		agindua.datuPertsonalakAldatu();
 	}
 	
-	public void kredituaGehitu(){
-		
+	public void kredituaGehitu(int pKreditua){
+		agindua.kredituaGehitu(pKreditua);
 	}
 	
-	public void pelikulaAlokatu(){
-		
+	public void pelikulaAlokatu(String pPelikula){
+		Pelikula pelikula = agindua.getPelikula(pPelikula);
+		if(pelikula == null){
+			//mezua
+		}else{
+			if(pelikula.alokatutaDago()){
+				//mezua
+			}else{
+			pelikula.bajaEman();
+			}
+		}
 	}
 	
-	public void pelikulaItzuli(){
-		
+	public void pelikulaItzuli(String pPelikula){
+		agindua.pelikulaItzuli(pPelikula);
+		Pelikula pelikula = agindua.getPelikula(pPelikula);
+		pelikula.altaEman();
 	}
 	
 }
+
